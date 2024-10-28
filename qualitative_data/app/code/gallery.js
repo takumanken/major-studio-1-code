6039e331d22b13f872c3f3b7cbbc2cf2ce080a04
tak-w <iamtakuhisam@gmail.com>
2024-10-27 23:17:19 -0400

    add filter function

--- qualitative_data/app/code/gallery.js
+++ qualitative_data/app/code/gallery.js
@@ -10,28 +10,109 @@ let stateObject = {
     selfPortrait: [],
 }
 
-async function InitializeStateObject(data) {
-    stateObject.genre        = aggregateCounts(data, d => d.categories[0]);
-    stateObject.gender       = aggregateCounts(data, d => d.sex);
-    stateObject.portraitYear = aggregateCounts(data, d => d.portraitYear.yearGroup).sort();
-    stateObject.age          = aggregateCounts(data, d => d.ageAtPortrait.ageGroup).sort();
-    stateObject.selfPortrait = aggregateCounts(data, d => d.isSelfPortrait).sort().reverse();
+async function initializeFilter(data) {
+
+    // Aggregate counts for each filter
+    function aggregateCounts(data, accessor) {
+        const rollup = d3.rollups(
+            data,
+            v => v.length,
+            accessor
+        );
+        const sorted = rollup.sort((a, b) => b[1] - a[1]);
+        return sorted.map(d => d[0]);
+    }
+    
+    // Populate filter options
+    function populateFilterOptions(data, elementId) {
+        d3.select(`#${elementId}`)
+            .selectAll("option")
+            .data(data)
+            .enter()
+            .append("option")
+            .text(d => d)
+            .attr("value", d => d);
+    }
+
+    aggGenre        = aggregateCounts(data, d => d.categories[0]);
+    aggGender       = aggregateCounts(data, d => d.sex);
+    aggPortraitYear = aggregateCounts(data, d => d.portraitYear.yearGroup);
+    aggAge          = aggregateCounts(data, d => d.ageAtPortrait.ageGroup);
+    aggSelfPortrait = aggregateCounts(data, d => d.isSelfPortrait);
+
+    // Update State Object
+    stateObject.genre        = aggGenre;
+    stateObject.gender       = aggGender;
+    stateObject.portraitYear = aggPortraitYear.sort();
+    stateObject.age          = aggAge.sort();
+    stateObject.selfPortrait = aggSelfPortrait.sort().reverse();
+
+    // Populate filter options
+    populateFilterOptions(stateObject.genre, "genreFilter");
+    populateFilterOptions(stateObject.gender, "genderFilter");
+    populateFilterOptions(stateObject.portraitYear, "yearFilter");
+    populateFilterOptions(stateObject.age, "ageFilter");
+    populateFilterOptions(stateObject.selfPortrait, "sfFilter");
+
 }
 
-function aggregateCounts(data, accessor) {
-    const rollup = d3.rollups(
-        data,
-        v => v.length,
-        accessor
+function updateStateObject() {
+
+    function getFilterValues(elementId) {
+        return Array.from(document.getElementById(elementId).selectedOptions)
+            .map(option => option.value.split(" (")[0]);
+    }
+
+    // Update state object
+    const currentGenre = getFilterValues("genreFilter");
+    if (currentGenre.length > 0) stateObject.genre = currentGenre;
+
+    const currentGender = getFilterValues("genderFilter");
+    if (currentGender.length > 0) stateObject.gender = currentGender;
+    
+    const currentPortraitYear = getFilterValues("yearFilter");
+    if (currentPortraitYear.length > 0) stateObject.portraitYear = currentPortraitYear;
+
+    const currentAge = getFilterValues("ageFilter");
+    if (currentAge.length > 0) stateObject.age = currentAge;
+
+    const currentSelfPortrait = getFilterValues("sfFilter");
+    if (currentSelfPortrait.length > 0) stateObject.selfPortrait = currentSelfPortrait;
+
+}
+
+function updateData(data) {
+
+    updateStateObject();
+
+    console.log("updateData", stateObject);
+
+    // Filter function
+    const filteredData = data.filter(d => 
+        stateObject.gender.includes(d.sex)
+        && stateObject.genre.includes(d.categories[0])
+        && stateObject.portraitYear.includes(d.portraitYear.yearGroup)
+        && stateObject.age.includes(d.ageAtPortrait.ageGroup)
+        && stateObject.selfPortrait.includes(d.isSelfPortrait)
     );
-    const sorted = rollup.sort((a, b) => b[1] - a[1]);
-    return sorted.map(d => `${d[0]} (${d[1]})`);
+
+    // let stateObject = {
+    //     genre: [],
+    //     gender: [],
+    //     portraitYear: [],
+    //     age: [],
+    //     selfPortrait: [],
+    // }
+    
+    // Update gallery
+    drawGallery(filteredData);
+
 }
 
 // -------------------
 // Data Loading
 // -------------------
-async function getGalleryData() {
+async function getAllGalleryData() {
     const response = await fetch('../data/data.json');
     return await response.json();
 }
@@ -42,6 +123,8 @@ async function getGalleryData() {
 function drawGallery(data) {
     const portraitWidth = 250;
     const gapBetweenPortraits = 5;
+
+    d3.select("#portrait-gallery").selectAll("*").remove();
 
     const gallery = d3.select("#portrait-gallery")
         .style("display", "grid")
@@ -103,18 +186,23 @@ function drawGallery(data) {
 // Helper Functions
 // -------------------
 function addScrollEventToToggleHeaderFooter() {
+    
     let isScrolling;
-    const header = document.getElementById('header');
-    const footer = document.getElementById('footer');
+    const header = d3.select("#header");
+    const footer = d3.select("#footer");
 
-    window.addEventListener("scroll", () => {
-        window.clearTimeout(isScrolling);
-        header.classList.add('hidden');
-        footer.classList.add('hidden');
+    d3.select(window).on("scroll", () => {
+        // Clear the previous timeout
+        clearTimeout(isScrolling);
 
+        // Hide header and footer while scrolling
+        header.classed("hidden", true);
+        footer.classed("hidden", true);
+
+        // Show header and footer again after scrolling stops
         isScrolling = setTimeout(() => {
-            header.classList.remove('hidden');
-            footer.classList.remove('hidden');
+            header.classed("hidden", false);
+            footer.classed("hidden", false);
         }, 500);
     });
 }
@@ -123,12 +211,13 @@ function addScrollEventToToggleHeaderFooter() {
 // Main
 // -------------------
 
-async function main() {
-    const data = await getGalleryData();
-    console.log(data);
-    await InitializeStateObject(data);
-    addScrollEventToToggleHeaderFooter();
+async function init() {
+    
+    const data = await getAllGalleryData();
     drawGallery(data);
+    await initializeFilter(data);
+    d3.selectAll(".filter").on("change", () => updateData(data));
+    addScrollEventToToggleHeaderFooter();
 }
 
-main();
+init();
